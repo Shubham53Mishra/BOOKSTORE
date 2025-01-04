8656805fbe9ce00eeb7fe379a945847c2c679173
Shubham53Mishra <21cse53@vmkvec.edu.in>
2025-01-04 20:44:48 +0530

    Update start script to use node instead of nodemon

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,9 +1,12 @@
-/** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui';
+
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    darkMode: "class",
-    theme: {
-        extend: {},
-    },
-    plugins: [require("daisyui")],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
 };
